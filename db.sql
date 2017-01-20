ecdbb7919b880ad3b71a52ab382b8d3dbd05de4c
abhigna20 <abhigna20@gmail.com>
2017-01-21 00:58:42 +0530

    database

---
+++ db.sql
@@ -0,0 +1,41 @@
+delimiter $$
+
+CREATE DATABASE `wingman_db` /*!40100 DEFAULT CHARACTER SET latin1 */$$
+
+delimiter $$
+
+CREATE TABLE `Customer_Details` (
+  `FirstName` varchar(100) DEFAULT NULL,
+  `LastName` varchar(100) DEFAULT NULL,
+  `Age` int(11) DEFAULT NULL,
+  `Email` varchar(45) DEFAULT NULL,
+  `Phone` varchar(45) DEFAULT NULL,
+  `DOB` varchar(45) DEFAULT NULL,
+  `Address` varchar(45) DEFAULT NULL,
+  `Username` varchar(45) NOT NULL,
+  `Password` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`Username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1$$
+
+delimiter $$
+
+CREATE TABLE `receiver_details` (
+  `Name` int(11) NOT NULL,
+  `Ddate` varchar(45) DEFAULT NULL,
+  `Email` varchar(45) DEFAULT NULL,
+  `Phone` varchar(45) DEFAULT NULL,
+  `Flightno` varchar(45) DEFAULT NULL,
+  `Weight` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1$$
+
+delimiter $$
+
+CREATE TABLE `sender_details` (
+  `Name` varchar(100) DEFAULT NULL,
+  `Ddate` varchar(45) DEFAULT NULL,
+  `Email` varchar(45) NOT NULL,
+  `Phone` varchar(45) DEFAULT NULL,
+  `Weight` int(11) DEFAULT NULL,
+  PRIMARY KEY (`Email`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1$$
+
